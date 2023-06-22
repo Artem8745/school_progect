c048fc684ba8bafbe0af84c23d3ec71b6826e2f0
Artem8745 <102891549+Artem8745@users.noreply.github.com>
2023-06-22 12:56:55 +0400

    Site

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle, .logo, a, .menu-list-item, .featured-content, .movie-list-item-img, .featured-content__text, .featured-desc, .featured-content__body, .menu-list__hover, .menu__box");
+const listitemstyle = document.querySelector(".menu-list-item");
+
+ball.addEventListener("click",()=>{
+    ball.classList.toggle("active")
+});
+
+
+
+
+$(function () {
+  $('.toggle-ball').on('click' ,function(event) {
+    $(this).toggleClass('active');
+    $('body').toggleClass('active');	
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+    ball.classList.toggle("active")
+
+    listitemstyle.style.color= "black"
+  })
+})
+
+if (localStorage.getItem('body') == 'active') {
+    $('.body').addClass('active');
+  }
+
+
+if ($.cookie('mysite_sheetswitch_idx')) {
+	var idx = $.cookie('mysite_sheetswitch_idx');
+	$(".switch[rel='stylesheet']").eq(idx).attr("disabled", "");
+	$(".swatch").eq(idx).addClass("swatch_hi");
+}
---
+++ script_diploms.js
@@ -0,0 +1,21 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    const itemNamber = movieLists[i].querySelectorAll("img").length
+    let clickCounter = 0;
+    arrow.addEventListener("click",()=>{
+        const ratio = Math.floor(window.innerWidth / 210);
+        clickCounter++;
+        if(itemNamber - (3 + clickCounter) + (4-ratio) >= 0) {
+            movieLists[i].style.transform = `translateX(${
+                movieLists[i].computedStyleMap().get("transform")[0].x.value -300
+            }px)`;
+        } else{
+            movieLists[i].style.transform = "translateX(0)"
+            clickCounter = 0;
+        }
+    });
+
+    console.log(movieLists[i].querySelectorAll("img").length)
+});
